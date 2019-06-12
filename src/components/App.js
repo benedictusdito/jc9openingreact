480b6972b08d3f651c430cb3118113877f7fe4c8
rochafialvin <rochafi.dev@gmail.com>
2019-06-12 10:43:07 +0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react' // ES6
+
+// Class based component
+// Function based component
+
+// component, file, class = Sama
+
+class App extends React.Component { // WAJIB RETURN SATU BUAH DIV
+    render(){
+        return <div><h1>App Component</h1></div> // bukan html, JSX
+    }
+}
+
+
+export default App
